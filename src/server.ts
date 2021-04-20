2a62b015a9835996b53d6b384639cbf9e47191af
Thiago Souza <thlima.silva.souza@gmail.com>
2021-04-20 08:37:23 -0300

    Configurando o typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import "./database";
+
 const app = express();
 
 /**
@@ -18,7 +20,7 @@ const app = express();
 
  app.post("/users", (request, response) => {
    return response.json({
-     message: "Usuário salvo com sucesso!"
+     message: "Usuário salvo com sucesso! "
    })
  })
 
