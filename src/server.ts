33d347147d196445c91a6e38447f058d388f28bb
Thiago Souza <thlima.silva.souza@gmail.com>
2021-04-22 08:35:12 -0300

    Aplicando configuraçao para utilizar o html

--- src/server.ts
+++ src/server.ts
@@ -18,4 +18,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+http.listen(3333, () => console.log("Server is running on port 3333"));
